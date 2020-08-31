fbf84bbf4b75eaecad94f8a0c2781b4d66ade669
Rubin <rubinradhakrishnan@gmail.com>
2020-09-01 01:29:31 +0530

    Add few images and the footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import "./App.css";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
       {/* Header */}
 
       {/* Banner */}
