6b30fdf7a203645383ee7021c9adfda532a1c947
Rubin <rubinradhakrishnan@gmail.com>
2020-08-31 23:52:42 +0530

    Add Card section in the home component

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Home.css";
 import Banner from "./Banner";
+import Card from "./Card";
 
 function Home() {
   return (
     <div className="home">
-      {/* <h1>Home Component</h1> */}
       <Banner />
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
