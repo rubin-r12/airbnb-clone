78a7da38fe7723aaf90fe62c63d83d959a1337d2
Rubin <rubinradhakrishnan@gmail.com>
2020-08-31 17:56:33 +0530

    Add Search bar field and few elements to the right of the header

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
   return (
@@ -9,6 +13,16 @@ function Header() {
         src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
         alt=""
       />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
     </div>
   );
 }
