d8cdddb870e429088268e916935bd1f8548dac1e
Rubin <rubinradhakrishnan@gmail.com>
2020-08-31 16:28:02 +0530

    Add the airbnb logo in the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./Home";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Home />
       {/* Header */}
 
