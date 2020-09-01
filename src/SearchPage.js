d87ae818406cb3034fbe749f64c29f42438efacb
Rubin <rubinradhakrishnan@gmail.com>
2020-09-01 18:04:46 +0530

    Add the filter buttons in the search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./SearchPage.css";
+import { Button } from "@material-ui/core";
 
 function SearchPage() {
   return (
     <div className="searchPage">
-      <div className="searchpage__info">
-        <p>62 stays . August 26th to August 30th . 2 guests</p>
+      <div className="searchPage__info">
+        <p>62 stays • August 26th to August 30th • 2 guests</p>
         <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of Place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
       </div>
     </div>
   );
